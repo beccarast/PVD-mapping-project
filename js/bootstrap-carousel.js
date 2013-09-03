a3dc1a440f414b5649612a4be3315817f7913f56
dlgarfield <daniel_garfield@brown.edu>
2013-09-03 16:41:25 -0400

    spelling change

--- js/bootstrap-carousel.js
+++ js/bootstrap-carousel.js
@@ -29,6 +29,8 @@
   var Carousel = function (element, options) {
     this.$element = $(element)
     this.options = options
+    this.interval    = false
+
     this.options.slide && this.slide(this.options.slide)
     this.options.pause == 'hover' && this.$element
       .on('mouseenter', $.proxy(this.pause, this))
